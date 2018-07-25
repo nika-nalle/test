0522fc7f158a73b3115151c6a36f2500415f21d5
Nika <0996745@gmail.com>
2018-07-25 16:58:08 +0300

    Это наш первый комит

---
+++ lesson 15 pro/script.js
@@ -0,0 +1,83 @@
+var name = document.getElementById("name");
+var age = document.getElementById("age");
+
+console.log(name);
+
+
+
+
+
+
+
+
+
+//alert("Hello world!");
+
+//console.log("Hello world!");
+
+/*var myFirstVar = 50;
+
+myFirstVar = 25;
+
+console.log(myFirstVar);*/
+
+/*console.log(4+6);
+console.log(4+ "I am strong");
+console.log("Hello" + " World");*/
+
+/*var incr = 10,
+				decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);*/
+
+//console.log(10%3);
+
+/*var num = "5"; //знак присваивания
+console.log(num == 5); // сравнение по значению != - не равен
+console.log(num === 5); // сравнение по типам*/
+
+/*var number = 5; // значение переменной - число
+var string = "Hello World!"; // значение переменной - строка
+var symbol; // значение переменной - символ
+true; // значение переменной - логический тип данных 
+false;
+
+console.log(6/2 == 3);
+console.log(10/5 == 3);
+
+var und;
+console.log(und);
+
+var obj = {};
+
+console.log(typeof obj); // typeof определяет какой вид переменной представлен
+console.log(typeof string);*/
+
+/*if ( 10 + 5 == 14) {
+				alert("It's a true!");
+}
+
+else if (10 + 5 == 15) {
+				alert("Wrong!");
+}*/
+
+/*for (var i = 1; i < 8; i++) {
+				console.log(i);
+};*/
+
+/*var out = 5;*/
+
+/*function myFirstFunc(x,y) {
+	var inner = 10;
+
+	//console.log(out);
+
+	return (x + y);
+}
+console.log(myFirstFunc(5,5));*/
+//console.log(inner);
+
